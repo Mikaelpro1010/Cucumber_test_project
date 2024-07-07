859183e387e6076982a63e23f4341dc3c92dbef5
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-07-07 20:48:56 -0300

    Criando a classe LoginPage para criação dos métodos de teste e importando as bibibliotecas para a realização automaticas dos testes através do navegador

---
+++ demo/src/test/java/cucumber/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package cucumber.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage extends BasePage {
+    
+}
