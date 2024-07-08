066529bc52e56f0e27983d71968dda167294376d
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-07-07 21:33:45 -0300

    Importando as bibliotecas referentes ao cenario de teste do cucumber e instanciando a classe LoginPage

--- demo/src/test/java/cucumber/StepDefinitions.java
+++ demo/src/test/java/cucumber/StepDefinitions.java
@@ -1,5 +1,14 @@
 package cucumber;
 
+import java.util.Random;
+
+import cucumber.pages.LoginPage;
+
+import io.cucubmer.java.en.Given;
+import io.cucubmer.java.en.Then;
+import io.cucubmer.java.en.When;
+
+
 public class StepDefinitions {
     
 }
